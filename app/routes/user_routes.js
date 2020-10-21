ce22770e04cc7a87c090fb3b5cf3edfbfc547767
aseem09 <aseemverma999@gmail.com>
2020-10-21 18:07:19 +0530

    Fix bug

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -4,7 +4,11 @@ const userController = require("../controllers/user_controller.js");
 
 module.exports = app => {
 
-  app.post('/users', userController.create);
-  app.get('/users', userController.getUsers);
+  app.post('/users', function (req, res) {
+    userController.Create
+  });
+  app.get('/users', function (req, res) {
+    userController.Create
+  });
 
 };
