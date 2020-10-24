dbced188a7cf3d601ffab40c6d44ad09cd7073ec
aseem09 <aseemverma999@gmail.com>
2020-10-24 14:57:56 +0530

    Remove auth service

--- app/services/user_service.js
+++ app/services/user_service.js
@@ -22,6 +22,19 @@ class UserService {
             result(null, res);
         });
     };
+
+    getUserFromId = (user, result) => {
+        sql.query('SELECT * FROM users WHERE user_name = ? AND user_password = ?;', [user.user_name, user.user_password], (err, res) => {
+            if (err) {
+                console.log("Error " + err);
+                result(err, null);
+                return;
+            }
+            console.log(res);
+            result(null, res);
+        });
+    };
+    
 }
 
 module.exports = new UserService();
