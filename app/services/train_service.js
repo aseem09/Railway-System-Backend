549bdd12e805b1a1177b994e064e54bcc4f8e600
gurpreet-singh135 <gsingh2@cs.iitr.ac.in>
2020-10-30 19:19:06 +0530

    Corrected train search query

--- app/services/train_service.js
+++ app/services/train_service.js
@@ -34,7 +34,26 @@ class TrainService {
         });
     };
     getSearchResults = (searchQuery,result)=> {
-        sql.query("SELECT train_number, arrival_time, departure_time, halt_time FROM destinations WHERE train_number IN (SELECT train_number FROM destinations WHERE destinations.station_name IN (?,?) GROUP BY train_number HAVING COUNT(DISTINCT station_name) = 2) AND station_name = ?;",[searchQuery.start, searchQuery.stop, searchQuery.start], (err, res) => {
+        sql.query("SELECT source.train_number as train_number , source.arrival_time as start_arrival_time, source.departure_time as start_departure_time, dest.arrival_time as stop_arrival_time, dest.departure_time as stop_departure_time\
+        FROM \
+        (SELECT *\
+        FROM destinations \
+        WHERE train_number \
+        IN (SELECT train_number \
+            FROM destinations \
+            WHERE destinations.station_name \
+            IN (?,?) \
+            GROUP BY train_number \
+            HAVING COUNT(DISTINCT station_name) = 2) AND station_name = ?) as source INNER JOIN\
+        (SELECT *\
+        FROM destinations \
+        WHERE train_number \
+        IN (SELECT train_number \
+            FROM destinations \
+            WHERE destinations.station_name \
+            IN (?,?) \
+            GROUP BY train_number \
+            HAVING COUNT(DISTINCT station_name) = 2) AND station_name = ?) as dest on source.train_number = dest.train_number;",[searchQuery.start, searchQuery.stop, searchQuery.start,searchQuery.start, searchQuery.stop, searchQuery.stop], (err, res) => {
             if (err) {
                 console.log("Error " + err);
                 result(err, null);
